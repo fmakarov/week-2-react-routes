bc13bdf1056846ce59532a1c59d5823c21a9db5d
fedro <fjodor.makaroff@gmail.com>
2020-05-17 14:04:28 -0700

    add homework fix

--- client/components/dashboard-main.js
+++ client/components/dashboard-main.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Head from './head'
+import Header from './header'
 
 const Main = () => {
   return (
     <div>
-      <Head title="Hello" />
+      <Header title="Hello" />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           <div id="title">Main</div>
--- client/components/dashboard-profile.js
+++ client/components/dashboard-profile.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 
-import Head from './head'
+import Header from './header'
 
 const Profile = () => {
   const { user } = useParams()
   return (
     <div>
-      <Head title="Hello" />
+      <Header title="Hello" />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           <Link to="/dashboard">Go To Root</Link>
